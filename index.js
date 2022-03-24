ff9c3e9b80f3ae83bbb56b6dbcb6d94e7e7ef4ce
Jonny Howle <jonny.howle@gmail.com>
2022-03-24 17:48:36 -0600

    commit tree 9be93a18e58d48c1719373afb455e78d0a895ef9

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks(pickupLocation) {
+    if (pickupLocation > hq) {
+        return pickupLocation - hq;
+    }
+    else if (pickupLocation < hq) {
+        return hq - pickupLocation;
+    }
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264;
+    } else if (start < destination) {
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    } else if ((distanceTravelledInFeet(start, destination) > 400) && (distanceTravelledInFeet(start, destination) <= 2000)) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    } else if ((distanceTravelledInFeet(start, destination) > 2000) && (distanceTravelledInFeet(start, destination) <= 2500)) {
+        return 25;
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return `cannot travel that far`;
+    }
+}
